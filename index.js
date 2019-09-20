6b1c24d6d3e725cb77b02c0f6334641a953039d3
zhibin <li.zhibin@scimall.org.cn>
2019-09-20 16:30:56 +0800

    修改版本号

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const init = require('./src/init');
 
 exports.main = () => {
     program
-        .version('0.0.1')
+        .version('0.0.2')
         .usage('<command> [options]');
 
     program
