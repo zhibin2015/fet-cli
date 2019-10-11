08c6a8686181f14ebfb267b4e59b68d650fd1c8a
zhibin <li.zhibin@scimall.org.cn>
2019-10-11 16:22:30 +0800

    新增模版

--- src/template.js
+++ src/template.js
@@ -1,8 +1,14 @@
 module.exports = {
-    list: [{
+    list: [
+        {
             "name": "template-zhuanti",
             "path": "git@gitlab.cspcbs.com:FE/template-zhuanti.git",
-            "desc": "专题开发基础模版可以，接入 Zepto 或 JQuery"
+            "desc": "专题开发基础模版，可以接入 Zepto 或 JQuery"
+        },
+        {
+            "name": "template-vue",
+            "path": "git@gitlab.cspcbs.com:FE/template-vue.git",
+            "desc": "基于 Vue 的项目工程模版"
         }
     ]
 };
